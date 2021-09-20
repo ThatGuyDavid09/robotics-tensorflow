2495f050789912e3e5e8eb6b541d7a7e73baefad
ThatGuyDavid09 <everythingexplained1234@gmail.com>
2021-09-20 18:25:56 -0400

    inital commit

---
+++ create_tflite_model.py
@@ -0,0 +1,19 @@
+from tflite_model_maker import image_classifier
+from tflite_model_maker.image_classifier import DataLoader
+
+# Load input data specific to an on-device ML app.
+data = DataLoader.from_folder(r"C:\Users\vadim\.keras\datasets\flower_photos")
+train_data, test_data = data.split(0.9)
+
+# Customize the TensorFlow model.
+model = image_classifier.create(train_data,
+                                epochs=15,
+                                batch_size=32,
+                                dropout_rate=0.2,
+                                shuffle=True)
+
+# Evaluate the model.
+loss, accuracy = model.evaluate(test_data)
+
+# Export to Tensorflow Lite model and label file in `export_dir`.
+model.export(export_dir='./tflite/')
